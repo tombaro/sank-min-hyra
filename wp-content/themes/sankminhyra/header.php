f540f825783bec89f05c3d68a129faf90c332704
Tom Baro <tom@klandestino.se>
2015-02-24 11:20:33 +0100

    Styling: Actually get full width

--- wp-content/themes/sankminhyra/header.php
+++ wp-content/themes/sankminhyra/header.php
@@ -12,37 +12,35 @@
 	<body <?php body_class(); ?>>
 
 	<div class="navbar-wrapper">
-		<div class="container-fluid">
 
-			<nav class="navbar navbar-inverse navbar-static-top">
-				<div class="container">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navigation" aria-expanded="false" aria-controls="navbar">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">Project name</a>
-					</div>
-					<div class="collapse navbar-collapse navbar-responsive-collapse" id="main-navigation">
-					<?php
-						if ( has_nav_menu( 'primary' ) ) :
-							wp_nav_menu( array(
-								
-								'theme_location'    => 'primary',
-								'depth'             => 2,
-								'container'         => 'false',
-								'menu_class'        => 'nav navbar-nav',
-								'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-								'walker'            => new wp_bootstrap_navwalker()
-								)
-							);
-						endif;
-					?>
+		<nav class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navigation" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Project name</a>
 				</div>
+				<div class="collapse navbar-collapse navbar-responsive-collapse" id="main-navigation">
+				<?php
+					if ( has_nav_menu( 'primary' ) ) :
+						wp_nav_menu( array(
+							
+							'theme_location'    => 'primary',
+							'depth'             => 2,
+							'container'         => 'false',
+							'menu_class'        => 'nav navbar-nav',
+							'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+							'walker'            => new wp_bootstrap_navwalker()
+							)
+						);
+					endif;
+				?>
 				</div>
-			</nav>
+			</div>
+		</nav>
 
-		</div>
 	</div>
--- wp-content/themes/sankminhyra/index.php
+++ wp-content/themes/sankminhyra/index.php
@@ -6,38 +6,36 @@ Template Name: Default page Template
 
 <?php get_header(); ?>
 
-	<div class="container-fluid">
-		<?php //while ( have_posts() ) : the_post(); ?>
-			<?php
-				$args = array(
-					'sort_order' => 'ASC',
-					'sort_column' => 'menu_order', //post_title
-					'hierarchical' => 1,
-					'exclude' => '',
-					'child_of' => 0,
-					'parent' => -1,
-					'exclude_tree' => '',
-					'number' => '',
-					'offset' => 0,
-					'post_type' => 'page',
-					'post_status' => 'publish'
-				);
-				$pages = get_pages($args);
-				//start loop
-				foreach ($pages as $page_data) {
-					$content = apply_filters('the_content', $page_data->post_content);
-					$title = $page_data->post_title;
-					$slug = $page_data->post_name;
-				?>
-					<div class='<?php echo "$slug" ?>' id="<?php echo $slug ?>">
-							<h2><?php echo "$title" ?></h2>
-								<?php echo "$content" ?>
-					</div>
+	<?php //while ( have_posts() ) : the_post(); ?>
+		<?php
+			$args = array(
+				'sort_order' => 'ASC',
+				'sort_column' => 'menu_order', //post_title
+				'hierarchical' => 1,
+				'exclude' => '',
+				'child_of' => 0,
+				'parent' => -1,
+				'exclude_tree' => '',
+				'number' => '',
+				'offset' => 0,
+				'post_type' => 'page',
+				'post_status' => 'publish'
+			);
+			$pages = get_pages($args);
+			//start loop
+			foreach ($pages as $page_data) {
+				$content = apply_filters('the_content', $page_data->post_content);
+				$title = $page_data->post_title;
+				$slug = $page_data->post_name;
+			?>
+				<div class='<?php echo "$slug" ?> container' id="<?php echo $slug ?>">
+					<h2><?php echo "$title" ?></h2>
+					<?php echo "$content" ?>
 					<a href="#top" rel="m_PageScroll2id">top</a>
-		<?php //endwhile; ?>
+				</div>
+				
+	<?php //endwhile; ?>
 
-		<?php } ?>
-
-	</div>
+	<?php } ?>
 	
 <?php get_footer(); ?>
